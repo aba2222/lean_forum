9e5058d4e5a8f93cec082ea47495bdd6a5cda4ee
aba2222 <ABCSSS2021@outlook.com>
2025-11-09 17:51:49 +0800

    Add logout

--- forum/urls.py
+++ forum/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -10,4 +11,5 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('register/', views.register_view, name='register'),
     path('about/', views.about_view, name='about'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
