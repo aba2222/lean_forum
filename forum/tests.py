6424267593ff6367dff26b0c3c3b01c2860a4e50
aba2222 <ABCSSS2021@outlook.com>
2026-02-06 15:00:39 +0800

    feat: 添加匿名用户无法创建帖子和仅作者可删除帖子的测试用例

--- forum/tests.py
+++ forum/tests.py
@@ -65,3 +65,32 @@ class ForumTests(TestCase):
         Rating.objects.create(user=self.user, item=self.item, score=3)
         Rating.objects.create(user=other, item=self.item, score=5)
         self.assertAlmostEqual(self.item.average_rating(), 4.0)
+
+    def test_anonymous_cannot_create_post(self):
+        url = reverse('post_create')
+        resp = self.client.post(url, {'title': 'NoAuth', 'content': 'Should not work'})
+        self.assertEqual(resp.status_code, 302)
+        self.assertFalse(Post.objects.filter(title='NoAuth').exists())
+
+    def test_post_delete_only_author(self):
+        other = User.objects.create_user('other2', password='p3')
+        post = Post.objects.create(author=self.user, title='to_delete', content='c')
+        # login as different user
+        self.client.login(username='other2', password='p3')
+        url = reverse('post_delete', kwargs={'pk': post.id})
+        resp = self.client.post(url)
+        # Other user should not be allowed to delete (404 from queryset filter)
+        self.assertEqual(resp.status_code, 404)
+        self.assertTrue(Post.objects.filter(id=post.id).exists())
+
+    def test_item_str_and_average_zero(self):
+        # Newly created item without ratings should report 0 average
+        self.assertAlmostEqual(self.item.average_rating(), 0)
+        self.assertIn('(avg: 0.0)', str(self.item))
+
+    def test_register_view_creates_user(self):
+        url = reverse('register')
+        resp = self.client.post(url, {'username': 'newuser', 'password': 'pw1', 'confirm_password': 'pw1'}, follow=True)
+        self.assertEqual(User.objects.filter(username='newuser').count(), 1)
+        new_user = User.objects.get(username='newuser')
+        self.assertTrue(new_user.check_password('pw1'))
