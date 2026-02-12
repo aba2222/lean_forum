aaa3f4e22eca254fce0444fc5bb6722d539aa872
aba2222 <ABCSSS2021@outlook.com>
2026-02-12 14:51:25 +0800

    feat: 增加ai bot基于open ai chat

--- forum/bots/bot.py
+++ forum/bots/bot.py
@@ -4,5 +4,5 @@ class BotBase():
         self.name = name
         self.id = id
 
-    def handler(self, post, context):
+    def handler(self, post, content):
         self.manager.send_comment(post, self.id, "Hello! I'm bot base.")
