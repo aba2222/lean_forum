a3f5895be2e44ba9d394bcd2a296cf26a39a2ad6
aba2222 <abcsss2035@gmail.com>
2026-07-09 16:59:55 +0800

    add corsheaders

--- lean_forum/settings.py
+++ lean_forum/settings.py
@@ -41,12 +41,14 @@ INSTALLED_APPS = [
     'django.contrib.humanize',
     'rest_framework',
     'rest_framework_simplejwt',
+    'corsheaders',
     'mdeditor',
     'webpush',
     'forum',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -55,6 +57,10 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOWED_ORIGINS = []
+
+CORS_ALLOW_ALL_ORIGINS = DEBUG  # Allow all origins in development mode
 
 ROOT_URLCONF = 'lean_forum.urls'
 
