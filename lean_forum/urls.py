5677fdd78d61320bf9f14d7d6e77ab9f3e71a577
aba2222 <abcsss2035@gmail.com>
2026-07-13 18:35:04 +0800

    update html

--- lean_forum/urls.py
+++ lean_forum/urls.py
@@ -26,7 +26,6 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path('admin/', admin.site.urls),
     re_path(r'^webpush/', include('webpush.urls')),
-    path('mdeditor/', include('mdeditor.urls')),
     path('', include('forum.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
